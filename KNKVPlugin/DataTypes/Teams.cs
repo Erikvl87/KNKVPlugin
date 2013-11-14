84b23f2cd6282d183a27f30ab40d199dfd78755d
Erik van Leeuwen <erikvl87@gmail.com>
2013-11-14 22:47:01 +0100

    hello, world

---
+++ KNKVPlugin/DataTypes/Teams.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections.Generic;
+using Newtonsoft.Json;
+
+namespace KNKVPlugin.DataTypes
+{
+	public class Teams
+	{
+		public Category Senioren { get; set; }
+		public Category Junioren { get; set; }
+		public Category Aspiranten { get; set; }
+		public Category Pupillen { get; set; }
+	}
+
+	public class Category
+	{
+		[JsonProperty(PropertyName = "v")]
+		public List<Team> Teams { get; set; }
+	}
+
+
+	public class Team
+	{
+		[JsonProperty(PropertyName = "season_year")]
+		public int SeasonYear { get; set; }
+
+		[JsonProperty(PropertyName = "season_serie")]
+		public string SeasonSerie { get; set; }
+
+		[JsonProperty(PropertyName = "club_id")]
+		public string ClubId { get; set; }
+
+		[JsonProperty(PropertyName = "poule_id")]
+		public int PouleId { get; set; }
+
+		[JsonProperty(PropertyName = "sport_id")]
+		public string SportId { get; set; }
+
+		[JsonProperty(PropertyName = "team_name")]
+		public string TeamName { get; set; }
+
+		[JsonProperty(PropertyName = "class_id")]
+		public int ClassId { get; set; }
+
+		[JsonProperty(PropertyName = "team_id")]
+		public int TeamId { get; set; }
+
+		[JsonProperty(PropertyName = "team_id_group")]
+		public string TeamIdGroup { get; set; }
+	}
+}
---
+++ KNKVPlugin/Request.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Specialized;
+using System.IO;
+using System.Net;
+using System.Text;
+using System.Web;
+using KNKVPlugin.DataTypes;
+using Newtonsoft.Json;
+
+namespace KNKVPlugin
+{
+	public class Request
+	{
+		private readonly WebRequest _request;
+
+		public Request(string code)
+		{
+			var serviceUrl = String.Format("http://www.knkv.nl/kcp/{0}/json/", code);
+			_request = WebRequest.Create(serviceUrl);
+			_request.ContentType = "application/x-www-form-urlencoded";
+			_request.Method = WebRequestMethods.Http.Post;
+		}
+
+
+		public Teams GetTeams()
+		{
+			var queryString = HttpUtility.ParseQueryString(String.Empty);
+			queryString["file"] = "json";
+			queryString["f"] = "get_data";
+			queryString["t"] = "teams";
+			queryString["t_id"] = "";
+			queryString["p"] = "0";
+			queryString["full"] = "0";
+			return JsonConvert.DeserializeObject<Teams>(Execute(queryString));
+		}
+
+
+		private string Execute(NameValueCollection postCollection)
+		{
+			var postData = postCollection.ToString();
+			var byteArray = new ASCIIEncoding().GetBytes(postData);
+			_request.ContentLength = byteArray.Length;
+
+			Stream stream = null;
+			StreamReader streamReader = null;
+			WebResponse response = null;
+
+			try
+			{
+				// POST
+				stream = _request.GetRequestStream();
+				stream.Write(byteArray, 0, byteArray.Length);
+				stream.Close();
+
+				// GET RESPONSE
+				response = _request.GetResponse();
+				stream = response.GetResponseStream();
+
+				// READ RESPONSE
+				streamReader = new StreamReader(stream);
+				var responseFromServer = streamReader.ReadToEnd();
+				return responseFromServer;
+				
+			}
+			finally
+			{
+				if (streamReader != null) 
+					streamReader.Close();
+
+				if (stream != null) 
+					stream.Close();
+
+				if (response != null) 
+					response.Close();
+			}
+		}
+	}
+}
