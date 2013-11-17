8c209346674b71ea1f8cce581595db0cfec20db6
Erik van Leeuwen <erikvl87@gmail.com>
2013-11-18 00:21:35 +0100

    Standings can now be requested

---
+++ KNKVPlugin/DataTypes/Standings.cs
@@ -0,0 +1,67 @@
+﻿using System.Collections.Generic;
+using Newtonsoft.Json;
+
+namespace KNKVPlugin.DataTypes
+{
+	public class Poule
+	{
+		[JsonProperty(PropertyName = "poule_name")]
+		public string PouleName { get; set; }
+
+		[JsonProperty(PropertyName = "sport")]
+		public string Sport { get; set; }
+	}
+
+	public class Position
+	{
+		[JsonProperty(PropertyName = "position")]
+		public string Pos { get; set; }
+
+		[JsonProperty(PropertyName = "team_name")]
+		public string TeamName { get; set; }
+
+		[JsonProperty(PropertyName = "played")]
+		public string Played { get; set; }
+
+		[JsonProperty(PropertyName = "points")]
+		public string Points { get; set; }
+
+		[JsonProperty(PropertyName = "won")]
+		public string Won { get; set; }
+
+		[JsonProperty(PropertyName = "lost")]
+		public string Lost { get; set; }
+
+		[JsonProperty(PropertyName = "draw")]
+		public string Draw { get; set; }
+
+		[JsonProperty(PropertyName = "sport")]
+		public string Sport { get; set; }
+
+		[JsonProperty(PropertyName = "serie")]
+		public string Serie { get; set; }
+
+		[JsonProperty(PropertyName = "goals_for")]
+		public string GoalsFor { get; set; }
+
+		[JsonProperty(PropertyName = "goals_against")]
+		public string GoalsAgainst { get; set; }
+
+		[JsonProperty(PropertyName = "penalties")]
+		public string Penalties { get; set; }
+	}
+
+	public class Standing
+	{
+		[JsonProperty(PropertyName = "poule")]
+		public Poule Poule { get; set; }
+
+		[JsonProperty(PropertyName = "lines")]
+		public List<Position> Positions { get; set; }
+	}
+
+	public class Standings
+	{
+		public List<Standing> Lists { get; set; } 
+	}
+}
--- KNKVPlugin/Request.cs
+++ KNKVPlugin/Request.cs
@@ -75,6 +75,36 @@ namespace KNKVPlugin
 		}
 
 
+		public Standings GetStandings()
+		{
+			var queryString = HttpUtility.ParseQueryString(String.Empty);
+			queryString["file"] = "json";
+			queryString["f"] = "get_data";
+			queryString["t"] = "standing";
+			queryString["t_id"] = "";
+			queryString["p"] = "0";
+			queryString["full"] = "0";
+
+			var response = Execute(queryString);
+
+			try
+			{
+				var jResponse = JArray.Parse(response);
+				var standings = new Standings {Lists = new List<Standing>()};
+
+				foreach (var row in jResponse)
+					standings.Lists.Add(JsonConvert.DeserializeObject<Standing>(row.ToString()));
+
+				return standings;
+			}
+			catch (JsonReaderException e)
+			{
+				// No valid JSON was recieved. Throw the ugly html error that the service is returning.
+				throw new ApplicationException(response, e);
+			}
+		}
+
+
 		private string Execute(NameValueCollection postCollection)
 		{
 			var request = WebRequest.Create(_serviceUrl);
