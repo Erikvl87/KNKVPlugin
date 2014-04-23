398151a0ffcbb7f7b8d443e71ff2ffb37df419ce
Erik van Leeuwen <erikvl87@gmail.com>
2014-04-23 21:26:11 +0200

    Added the ability to retrieve the full program instead of only the program for the current week.

--- KNKVPlugin/GetProgram.cs
+++ KNKVPlugin/GetProgram.cs
@@ -7,28 +7,59 @@ using Newtonsoft.Json.Linq;
 
 namespace KNKVPlugin
 {
+	/// <summary>
+	/// Returns the program for the teams of the current club (the club is determined by the service using the service code)
+	/// </summary>
 	public partial class Request
 	{
 		/// <summary>
-		/// Returns the program for the teams of the current club (the club is determined by the service using the service code)
+		/// Get the program for all teams for this week
 		/// </summary>
 		/// <returns></returns>
 		public Program GetProgram()
 		{
-			return GetProgram(null);
+			return GetProgram(null, false);
 		}
+
+
+
+		/// <summary>
+		/// Get the program
+		/// </summary>
+		/// <param name="fullProgram">Indication for returning the full program or only for the current week.</param>
+		/// <returns></returns>
+		public Program GetProgram(bool fullProgram)
+		{
+			return GetProgram(null, true);
+		}
+
+
+
+		/// <summary>
+		/// Get the program
+		/// </summary>
+		/// <param name="teamIds">Select teams to request the program</param>
+		/// <returns></returns>
+		public Program GetProgram(int[] teamIds)
+		{
+			return GetProgram(teamIds, false);
+		}
+
+
 
 		/// <summary>
 		/// Returns the poule program for the given teams
 		/// </summary>
+		/// <param name="teamIds">Select teams to request the program</param>
+		/// <param name="fullProgram">Indication for returning the full program or only for the current week.</param>
 		/// <returns></returns>
-		public Program GetProgram(int[] teamIds)
+		public Program GetProgram(int[] teamIds, bool fullProgram)
 		{
 			var queryString = HttpUtility.ParseQueryString(String.Empty);
 			queryString["t"] = "program";
 			queryString["t_id"] = (teamIds != null) ? String.Join(",", teamIds) : String.Empty;
 			queryString["p"] = "0";
-			queryString["full"] = "0";
+			queryString["full"] = (fullProgram) ? "1" : "0";
 
 			var response = Execute(queryString);
 
